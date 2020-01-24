c409de25a92c90434f9b63dd2429997e09be4394
exzhibit97 <exzhibit97@gmail.com>
2020-01-24 15:15:21 +0100

    Add project files.

---
+++ FlashcardsAPI/Models/Deck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashcardsAPI.Models
+{
+    public class Deck
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public List<Flashcard> Flashcards { get; set; }
+    }
+}
---
+++ FlashcardsAPI/Models/Flashcard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashcardsAPI.Models
+{
+    public class Flashcard
+    {
+        public int Id { get; set; }
+        public string CardFront { get; set; }
+        public string CardBack { get; set; }
+        public string CardDescription { get; set; }
+
+        public Flashcard() { }
+
+    }
+}
